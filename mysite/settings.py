7f5060f27ad829bc1504a58b8f1860fe5064d8d3
kmh357 <kmh357@gmail.com>
2019-06-28 15:58:35 +0900

    allowed host modify

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6yu*d)sqb8xa4b=3p2dm=rdi*w2pwd8%koh2n^@j)cz7@eif=e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['kmh357.pythonanywhere.com', 'localhost']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '.pythonanywhere.com']
 
 
 # Application definition
